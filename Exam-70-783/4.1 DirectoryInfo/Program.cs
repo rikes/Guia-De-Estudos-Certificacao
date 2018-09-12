7448aaf14a66f02a2c459e5959ed9c76fe12565f
rickes <henricke.ss@gmail.com>
2018-09-12 00:13:00 -0300

    4.1 metade

--- Exam-70-783/4.1 DirectoryInfo/Program.cs
+++ Exam-70-783/4.1 DirectoryInfo/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using System.Diagnostics;
 using System.IO;
+using System.Security.AccessControl;
 
 namespace _4._1_DirectoryInfo
 {
@@ -20,6 +21,35 @@ namespace _4._1_DirectoryInfo
             {
                 Debug.WriteLine(di.Name);
             }
+            //Criar diretorios
+            var directory = Directory.CreateDirectory(@"C:\Temp2\Directory");
+
+            var directoryCreate = new DirectoryInfo(@"C:\Temp2\DirectoryInfo");
+            directoryCreate.Create();
+
+            //Deletar
+            /*if (Directory.Exists(@"C:\Temp2\Directory"))
+            {
+                Console.WriteLine("Deletando pasta 'C:\\Temp2\\Directory'");
+                Directory.Delete(@"C:\Temp2\Directory");
+            }
+
+            if (directoryCreate.Exists)
+            {
+                directoryCreate.Delete();
+            }*/
+            System.Security.Principal.SecurityIdentifier sid = new System.Security.Principal.SecurityIdentifier(System.Security.Principal.WellKnownSidType.WorldSid, null);
+            System.Security.Principal.NTAccount acct = sid.Translate(typeof(System.Security.Principal.NTAccount)) as System.Security.Principal.NTAccount;
+            string strEveryoneAccount = acct.ToString();
+            //Controle de acesso as pastas
+            //No caso abaixo não permitir a deleção de uma determinada pasta por um determinado usuario
+            DirectorySecurity directorySecurity = directoryInfo.GetAccessControl();
+            directorySecurity.AddAccessRule(new FileSystemAccessRule(@"DESKTOP\FulanoX", FileSystemRights.Delete, AccessControlType.Deny));
+
+            directoryCreate.SetAccessControl(directorySecurity);
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Exam-70-783/4.1 DriverInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _4._1_DriverInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                DriveInfo[] drivesInfo = DriveInfo.GetDrives();
+
+                foreach (DriveInfo driveInfo in drivesInfo)
+                {
+                    Console.WriteLine("Drive {0}", driveInfo.Name);
+                    Console.WriteLine("File type: {0}", driveInfo.DriveType);
+                    Console.WriteLine("Raiz do diretório {0}", driveInfo.RootDirectory);
+
+                    //Verifica se o drive está disponível - Caso não esteja um null pointer pode ser lançado.
+                    if (driveInfo.IsReady == true)
+                    {
+                        Console.WriteLine("Volume label: {0}", driveInfo.VolumeLabel);
+                        Console.WriteLine("File system: {0}", driveInfo.DriveFormat);
+                        Console.WriteLine("Drive Type: {0}", driveInfo.DriveType);
+                        //bytes -> kb -> mb 
+                        Console.WriteLine("Available space to current user: {0, 15} mb", ((driveInfo.AvailableFreeSpace)/1024)/1024);
+                        Console.WriteLine("Total available space: {0, 15} mb", ((driveInfo.TotalFreeSpace) / 1024)/ 1024);
+                        Console.WriteLine("Total size of drive: {0, 15} mb", ((driveInfo.TotalSize) / 1024)/ 1024);
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
